e719d7e099c3befd9ffd1caab7ae5f218baf5d81
Fredrick Makoffu <fredrick@solutech.co.ke>
2023-10-29 12:09:41 +0300

    Added Badge ID to User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,8 +73,7 @@ class User extends Authenticatable
 
     public function achievements()
     {
-        return $this->belongsToMany(Achievement::class, 'user_achievements', 'user_id', 'achievement_id')
-            ->withPivot('achieved_at'); // Include the pivot table's "achieved_at" column, if applicable
+        return $this->belongsToMany(Achievement::class, 'user_achievements', 'user_id', 'achievement_id');
     }
 
     public function badge()
@@ -82,4 +81,3 @@ class User extends Authenticatable
         return $this->belongsTo(Badge::class);
     }
 }
-
